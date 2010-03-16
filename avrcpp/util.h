679cd7dcf2e87f94ed3ccecd2c1f6c3d0ae21a97
kchizhov <k.chizhov@mirantis.ru>
2010-03-16 14:52:16 +0300

    DisableInterrupts class and ATOMIC macro

--- avrcpp/util.h
+++ avrcpp/util.h
@@ -304,5 +304,25 @@ public:
 	}
 };
 
+class DisableInterrupts
+{
+public:
+	DisableInterrupts()
+	{
+		_sreg = SREG;
+		cli();
+	}
+	~DisableInterrupts()
+	{
+		SREG = _sreg;
+	}
+	operator bool()
+	{return false;}
+private:
+	uint8_t _sreg;
+};
+
+#define ATOMIC if(DisableInterrupts di = DisableInterrupts()){}else
+
 #endif
  
