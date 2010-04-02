d8e52f52a30f8ab1813f11502c9e71e8e716a8e6
kchizhov <k.chizhov@mirantis.ru>
2010-04-02 20:13:00 +0400

    bldc in progress

--- Bldc/bldc.cpp
+++ Bldc/bldc.cpp
@@ -78,6 +78,63 @@ protected:
 };
 
 
+class Comparator
+{
+protected:
+	enum{AdmuxMask = (1<<MUX0)|(1<<MUX1)|(1<<MUX2)|(1<<MUX3)};
+	uint8_t& Control()
+	{
+		return ACSR;
+	}
+	uint8_t& Multiplexer()
+	{
+		return ADMUX;
+	}
+	void MuxEnable()
+	{
+		SFIOR |= (1<<ACME);
+	}
+
+	void MuxDisable()
+	{
+		SFIOR &= ~(1<<ACME);
+	}
+
+public:
+	enum ComparatorChannel {AIN1=255, ADC0=0, ADC1=1, ADC2=2, ADC3=3, ADC4=4, ADC5=6, ADC6=6, ADC7=7, ADC8=8, ADC9=1, ADC10=10};
+	enum InterruptMode{Togle=0; Falling=2, Rising=3};
+
+	void SetChannel(ComparatorChannel ch)
+	{
+		if(ch == AIN1)
+		{
+			MuxDisable();
+		}
+		else
+		{
+			MuxEnable();
+			Multiplexer() = Multiplexer() & AdmuxMask | ch;
+		}
+	}
+
+	void EnableInterrupt(InterruptMode mode)
+	{
+		Control() |= (1 << ACIE) | (mode << ACIS0);
+	}
+	
+	uint8_t IsSet()
+	{
+		return Control() & (1<<ACO);
+	}
+};
+
+
+
+ISR(ANA_COMP_vect)
+{
+
+}
+
 
 int main()
 {
