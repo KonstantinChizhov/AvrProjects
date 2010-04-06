eafbd5abae9218427c35e75d8b5090ddb68a1047
kchizhov <k.chizhov@mirantis.ru>
2010-04-06 17:39:47 +0400

    ring buffer tuning

--- avrcpp/containers.h
+++ avrcpp/containers.h
@@ -25,64 +25,68 @@ class RingBuffer
 {
 protected:
 	typedef typename SelectSizeT<SIZE < 255>::Result INDEX_T;
-	RingBuffer()
-	{
-		Clear();
-	}
 
-	void Write(DATA_T c)
+	//NOTE: If uncommented, it will increase global constructors section considerably,
+	//		if this class is used as a glogal static object.
+	//		However, we can assume that buffer already cleaned at startup.
+	//RingBuffer()
+	//{
+	//	Clear();
+	//}
+
+	inline void Write(DATA_T c)
 	{
 		_data[(INDEX_T)(_writeCount++ & (INDEX_T)(SIZE-1))]=c;
 	}
 
-	DATA_T Read()
+	inline DATA_T Read()
 	{
 		return _data[(INDEX_T)(_readCount++ & (INDEX_T)(SIZE-1))];
 	}
 
 public:
 
-	DATA_T First()const
+	inline DATA_T First()const
 	{
 		return operator[](0);
 	}
 
-	DATA_T Last()const
+	inline DATA_T Last()const
 	{
 		return operator[](Size());
 	}
 
-	DATA_T& operator[] (INDEX_T i)
+	inline DATA_T& operator[] (INDEX_T i)
 	{
 		if(IsEmpty())
 			return DATA_T();
 		return _data[(INDEX_T)((_readCount + i) & (INDEX_T)(SIZE-1))];
 	}
 
-	const DATA_T& operator[] (INDEX_T i)const
+	inline const DATA_T& operator[] (INDEX_T i)const
 	{
 		if(IsEmpty())
 			return DATA_T();
 		return _data[(INDEX_T)((_readCount + i) & (INDEX_T)(SIZE-1))];
 	}
 
-	bool IsEmpty()const
+	inline bool IsEmpty()const
 	{
 		return (_writeCount == _readCount);
 	}
 		
-	bool IsFull()const
+	inline bool IsFull()const
 	{
 		return ((INDEX_T)(_writeCount - _readCount) & (INDEX_T)~(SIZE-1)) != 0;
 	}
 
-	void Clear()
+	inline void Clear()
 	{
 		_readCount=0;
     	_writeCount=0;
 	}
 
-	unsigned Size()
+	inline unsigned Size()
 	{return SIZE;}
 protected:
     DATA_T _data[SIZE];
@@ -97,24 +101,23 @@ class Queue :public RingBuffer<SIZE, DATA_T>
 {
 	using typename RingBuffer<SIZE, DATA_T>::INDEX_T;
 	using RingBuffer<SIZE, DATA_T>::IsFull;
-	using RingBuffer<SIZE, DATA_T>::Write;
 	using RingBuffer<SIZE, DATA_T>::IsEmpty;
-	using RingBuffer<SIZE, DATA_T>::Read;
+	
 
 public:
-	bool Write(DATA_T c)
+	inline bool Write(DATA_T c)
 	{
 		if(IsFull())
 			return 0;
-		Write(c);
+		RingBuffer<SIZE, DATA_T>::Write(c);
 		return 1;
 	}
 
-	bool Read(DATA_T &c)
+	inline bool Read(DATA_T &c)
 	{
 		if(IsEmpty())
 			return 0;
-		c=Read();
+		c=RingBuffer<SIZE, DATA_T>::Read();
 		return 1;
 	}
 };
@@ -124,23 +127,21 @@ class WrappingQueue :public RingBuffer<SIZE, DATA_T>
 {
 	using typename RingBuffer<SIZE, DATA_T>::INDEX_T;
 	using RingBuffer<SIZE, DATA_T>::IsFull;
-	using RingBuffer<SIZE, DATA_T>::Write;
 	using RingBuffer<SIZE, DATA_T>::IsEmpty;
-	using RingBuffer<SIZE, DATA_T>::Read;
 public:
-	bool Write(DATA_T c)
+	inline bool Write(DATA_T c)
 	{
 		if(IsFull())
 			RingBuffer<SIZE, DATA_T>::_readCount++;
-		Write(c);
+		RingBuffer<SIZE, DATA_T>::Write(c);
 		return 1;
 	}
 
-	bool Read(DATA_T &c)
+	inline bool Read(DATA_T &c)
 	{
 		if(IsEmpty())
 			return 0;
-		c=Read();
+		c=RingBuffer<SIZE, DATA_T>::Read();
 		return 1;
 	}
 };
@@ -152,12 +153,12 @@ class Array
 public:
 	unsigned Size()
 	{return SIZE;}
-	T& operator[](INDEX_T i)
+	inline T& operator[](INDEX_T i)
 	{
 		return _data[i];
 	}
 
-	const T& operator[](INDEX_T i)const
+	inline const T& operator[](INDEX_T i)const
 	{
 		return _data[i];
 	}
