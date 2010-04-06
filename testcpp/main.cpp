0d9ce66bae124a4b1510af4a856a5db12c574628
kchizhov <k.chizhov@mirantis.ru>
2010-04-06 17:42:11 +0400

    template dispatcher test

--- testcpp/main.cpp
+++ testcpp/main.cpp
@@ -1,23 +1,50 @@
-//#include <avr/io.h>
-//#include "dispatcher.h"
-//#include "usart.h"
-//#include "adc.h"
-//#include "HD44780.h"
-//#include "stepper.h"
-//#include "LedDisplay.h"
-//#include "util.h"
+#include "dispatcher.h"
 #include "ports.h"
+
+enum{ TIMER_INTERVAL = 187};
+enum{ TIMER_START = (256-TIMER_INTERVAL)};
+
+typedef Dispatcher<16, 10> dispatcher;
+
+ISR(TIMER0_OVF_vect)
+{
+	TCNT0 = TIMER_START;
+	dispatcher::TimerHandler();
+}
+
+void Timer0Init()
+{
+	TCNT0 = TIMER_START;
+	TCCR0 = (0<<CS02) | (1<<CS01) | (1<<CS00);
+	TIMSK |= (1<<TOIE0);
+}
+
+void F2();
+
+void F1()
+{
+	dispatcher::SetTimer(F2, 100);
+	Pa0::Set();
+}
+
+void F2()
+{
+	dispatcher::SetTimer(F1, 100);
+	Pa0::Clear();
+}
 
 
 int main()
 {
- 	typedef PinList<Pa0, Pa1, Pa2, Pa3, Pb5, Pb4, Pb2> pins;
-
-	pins::Write(PORTC);
-
+	dispatcher::Init();
+	Timer0Init();
+	Pa0::SetDirWrite();
+	sei();
+	F1();
+	
 	while(1)
 	{	
-	
+		dispatcher::Poll();
 	}
 
 }
