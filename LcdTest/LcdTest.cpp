59b0cc2ab7853642f19e02fad20e2bd97007b139
kchizhov <k.chizhov@mirantis.ru>
2010-06-29 14:22:18 +0400

    LCD sample updated

--- LcdTest/LcdTest.cpp
+++ LcdTest/LcdTest.cpp
@@ -1,40 +1,16 @@
 
 #include "HD44780.h"
-#include "ports.h"
+#include "iopins.h"
+#include "pinlist.h"
 
-#if 0
-Lcd
-<
-	TPin<Porta, 6>,
-	TPin<Porta, 5>,
-	TPin<Porta, 4>,
-	PinsLcdDataBus
-	<
-		TPin<Porta, 3>,
-		TPin<Porta, 2>,
-		TPin<Porta, 1>,
-		TPin<Porta, 0>
-	>
-> lcd;
+using namespace IO;
 
-#else
-
-Lcd
-<
-	TPin<Porta, 6>,
-	TPin<Porta, 5>,
-	TPin<Porta, 4>,
-	PortLcdDataBus
-	<
-		Porta, 0
-	>
-> lcd;
-
-#endif
+typedef Lcd<PinList<Pa0, Pa1, Pa2, Pa3, Pa4, Pa5, Pa6> > Lcd1;
 
 int main()
 {
- 	lcd.Puts("1234567890abcdef", 16);
+	Lcd1::Init();
+ 	Lcd1::Puts("1234567890abcdef", 16);
 	while(1)
 	{	
 	
