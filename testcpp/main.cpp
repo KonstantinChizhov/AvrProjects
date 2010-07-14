724e4ca2dc698e52a814ea10fedf4fdcf07a9653
kchizhov <kchizhov@griddinamics.com>
2010-07-14 12:51:04 +0400

    sample updated

--- testcpp/main.cpp
+++ testcpp/main.cpp
@@ -2,30 +2,20 @@
 #include "iopins.h"
 #include "pinlist.h"
 #include <avr/delay.h>
+#include "LedDisplay.h"
 
 using namespace IO;
 
-typedef Pa1 Led1;
-typedef Pa2 Led2;
+typedef PinList<Pa1, Pa2, Pb4, Pb5, Pc0, Pa0, Pa3> MyPins;
 
-template <class Led>
-void Blink()
-{
-	Led::Set();
-	_delay_ms(100);
-	Led::Clear();
-	_delay_ms(100);
-}
 
 int main()
 {
-	Led1::SetDirWrite();
-	Led2::SetDirWrite();
 
+	MyPins::DirWrite(0xff);
 	while(1)
 	{	
-		Blink<Led1>();
-		Blink<Led2>();
+		MyPins::Write(0x55);
 	}
 }
 
