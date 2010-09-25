f0bda69bef727a9ac9520162802b7f7f065c9aa4
Konstantin Chizhov <root@.(none)>
2010-09-25 21:32:44 +0400

    short pins definition updated

--- avrcpp/iopins.h
+++ avrcpp/iopins.h
@@ -4,7 +4,7 @@
 // Title		: C++ IO for ATMEL AVR
 // Author		: Konstantin Chizhov
 // Date			: 2010
-// Target MCU	: Atmel mega AVR, TINY avr AND Xmega Series
+// Target MCU	: Atmel MEGA, TINY, XMega AVR, TI Msp430
 //
 //       This code is distributed under the GNU Public License
 //       which can be found at http://www.gnu.org/licenses/gpl.txt
@@ -23,140 +23,136 @@
 
 #include "iopin.h"
 #include "ioports.h"
-//#include <avr/io.h>
+
+#define DECLARE_PORT_PINS(PORT_TYPE_NAME, PIN_NAME_PREFIX) \
+	typedef TPin<PORT_TYPE_NAME, 0> PIN_NAME_PREFIX ## 0;\
+	typedef TPin<PORT_TYPE_NAME, 1> PIN_NAME_PREFIX ## 1;\
+	typedef TPin<PORT_TYPE_NAME, 2> PIN_NAME_PREFIX ## 2;\
+	typedef TPin<PORT_TYPE_NAME, 3> PIN_NAME_PREFIX ## 3;\
+	typedef TPin<PORT_TYPE_NAME, 4> PIN_NAME_PREFIX ## 4;\
+	typedef TPin<PORT_TYPE_NAME, 5> PIN_NAME_PREFIX ## 5;\
+	typedef TPin<PORT_TYPE_NAME, 6> PIN_NAME_PREFIX ## 6;\
+	typedef TPin<PORT_TYPE_NAME, 7> PIN_NAME_PREFIX ## 7;\
+	typedef TPin<PORT_TYPE_NAME, 8> PIN_NAME_PREFIX ## 8;\
+	typedef TPin<PORT_TYPE_NAME, 9> PIN_NAME_PREFIX ## 9;\
+	typedef TPin<PORT_TYPE_NAME, 10> PIN_NAME_PREFIX ## 10;\
+	typedef TPin<PORT_TYPE_NAME, 11> PIN_NAME_PREFIX ## 11;\
+	typedef TPin<PORT_TYPE_NAME, 12> PIN_NAME_PREFIX ## 12;\
+	typedef TPin<PORT_TYPE_NAME, 13> PIN_NAME_PREFIX ## 13;\
+	typedef TPin<PORT_TYPE_NAME, 14> PIN_NAME_PREFIX ## 14;\
+	typedef TPin<PORT_TYPE_NAME, 15> PIN_NAME_PREFIX ## 15;\
+	typedef TPin<PORT_TYPE_NAME, 16> PIN_NAME_PREFIX ## 16;\
+	typedef TPin<PORT_TYPE_NAME, 17> PIN_NAME_PREFIX ## 17;\
+	typedef TPin<PORT_TYPE_NAME, 18> PIN_NAME_PREFIX ## 18;\
+	typedef TPin<PORT_TYPE_NAME, 19> PIN_NAME_PREFIX ## 19;\
+	typedef TPin<PORT_TYPE_NAME, 20> PIN_NAME_PREFIX ## 20;\
+	typedef TPin<PORT_TYPE_NAME, 21> PIN_NAME_PREFIX ## 21;\
+	typedef TPin<PORT_TYPE_NAME, 22> PIN_NAME_PREFIX ## 22;\
+	typedef TPin<PORT_TYPE_NAME, 23> PIN_NAME_PREFIX ## 23;\
+	typedef TPin<PORT_TYPE_NAME, 24> PIN_NAME_PREFIX ## 24;\
+	typedef TPin<PORT_TYPE_NAME, 25> PIN_NAME_PREFIX ## 25;\
+	typedef TPin<PORT_TYPE_NAME, 26> PIN_NAME_PREFIX ## 26;\
+	typedef TPin<PORT_TYPE_NAME, 27> PIN_NAME_PREFIX ## 27;\
+	typedef TPin<PORT_TYPE_NAME, 28> PIN_NAME_PREFIX ## 28;\
+	typedef TPin<PORT_TYPE_NAME, 29> PIN_NAME_PREFIX ## 29;\
+	typedef TPin<PORT_TYPE_NAME, 30> PIN_NAME_PREFIX ## 30;\
+	typedef TPin<PORT_TYPE_NAME, 31> PIN_NAME_PREFIX ## 31;
+
 
 namespace IO
 {
-	//Short pin definations 
+	//Short pin definations
+
 	#ifdef USE_PORTA
-	typedef TPin<Porta, 0> Pa0;
-	typedef TPin<Porta, 1> Pa1;
-	typedef TPin<Porta, 2> Pa2;
-	typedef TPin<Porta, 3> Pa3;
-	typedef TPin<Porta, 4> Pa4;
-	typedef TPin<Porta, 5> Pa5;
-	typedef TPin<Porta, 6> Pa6;
-	typedef TPin<Porta, 7> Pa7;
+	DECLARE_PORT_PINS(Porta, Pa)
 	#endif
 
 	#ifdef USE_PORTB
-	typedef TPin<Portb, 0> Pb0;
-	typedef TPin<Portb, 1> Pb1;
-	typedef TPin<Portb, 2> Pb2;
-	typedef TPin<Portb, 3> Pb3;
-	typedef TPin<Portb, 4> Pb4;
-	typedef TPin<Portb, 5> Pb5;
-	typedef TPin<Portb, 6> Pb6;
-	typedef TPin<Portb, 7> Pb7;
+	DECLARE_PORT_PINS(Portb, Pb)
 	#endif
 
 	#ifdef USE_PORTC
-	typedef TPin<Portc, 0> Pc0;
-	typedef TPin<Portc, 1> Pc1;
-	typedef TPin<Portc, 2> Pc2;
-	typedef TPin<Portc, 3> Pc3;
-	typedef TPin<Portc, 4> Pc4;
-	typedef TPin<Portc, 5> Pc5;
-	typedef TPin<Portc, 6> Pc6;
-	typedef TPin<Portc, 7> Pc7;
+	DECLARE_PORT_PINS(Portc, Pc)
 	#endif
 
 	#ifdef USE_PORTD
-	typedef TPin<Portd, 0> Pd0;
-	typedef TPin<Portd, 1> Pd1;
-	typedef TPin<Portd, 2> Pd2;
-	typedef TPin<Portd, 3> Pd3;
-	typedef TPin<Portd, 4> Pd4;
-	typedef TPin<Portd, 5> Pd5;
-	typedef TPin<Portd, 6> Pd6;
-	typedef TPin<Portd, 7> Pd7;
+	DECLARE_PORT_PINS(Portd, Pd)
 	#endif
 
 	#ifdef USE_PORTE
-	typedef TPin<Porte, 0> Pe0;
-	typedef TPin<Porte, 1> Pe1;
-	typedef TPin<Porte, 2> Pe2;
-	typedef TPin<Porte, 3> Pe3;
-	typedef TPin<Porte, 4> Pe4;
-	typedef TPin<Porte, 5> Pe5;
-	typedef TPin<Porte, 6> Pe6;
-	typedef TPin<Porte, 7> Pe7;
+	DECLARE_PORT_PINS(Porte, Pe)
 	#endif
 
 	#ifdef USE_PORTF
-	typedef TPin<Portf, 0> Pf0;
-	typedef TPin<Portf, 1> Pf1;
-	typedef TPin<Portf, 2> Pf2;
-	typedef TPin<Portf, 3> Pf3;
-	typedef TPin<Portf, 4> Pf4;
-	typedef TPin<Portf, 5> Pf5;
-	typedef TPin<Portf, 6> Pf6;
-	typedef TPin<Portf, 7> Pf7;
+	DECLARE_PORT_PINS(Portf, Pf)
 	#endif
 
 	#ifdef USE_PORTG
-	typedef TPin<Portg, 0> Pg0;
-	typedef TPin<Portg, 1> Pg1;
-	typedef TPin<Portg, 2> Pg2;
-	typedef TPin<Portg, 3> Pg3;
-	typedef TPin<Portg, 4> Pg4;
-	typedef TPin<Portg, 5> Pg5;
-	typedef TPin<Portg, 6> Pg6;
-	typedef TPin<Portg, 7> Pg7;
+	DECLARE_PORT_PINS(Portg, Pg)
 	#endif
 
 	#ifdef USE_PORTH
-	typedef TPin<Porth, 0> Ph0;
-	typedef TPin<Porth, 1> Ph1;
-	typedef TPin<Porth, 2> Ph2;
-	typedef TPin<Porth, 3> Ph3;
-	typedef TPin<Porth, 4> Ph4;
-	typedef TPin<Porth, 5> Ph5;
-	typedef TPin<Porth, 6> Ph6;
-	typedef TPin<Porth, 7> Ph7;
+	DECLARE_PORT_PINS(Porth, Ph)
 	#endif
 
 	#ifdef USE_PORTJ
-	typedef TPin<Portj, 0> Pj0;
-	typedef TPin<Portj, 1> Pj1;
-	typedef TPin<Portj, 2> Pj2;
-	typedef TPin<Portj, 3> Pj3;
-	typedef TPin<Portj, 4> Pj4;
-	typedef TPin<Portj, 5> Pj5;
-	typedef TPin<Portj, 6> Pj6;
-	typedef TPin<Portj, 7> Pj7;
+	DECLARE_PORT_PINS(Portj, Pj)
 	#endif
 
 	#ifdef USE_PORTK
-	typedef TPin<Portk, 0> Pk0;
-	typedef TPin<Portk, 1> Pk1;
-	typedef TPin<Portk, 2> Pk2;
-	typedef TPin<Portk, 3> Pk3;
-	typedef TPin<Portk, 4> Pk4;
-	typedef TPin<Portk, 5> Pk5;
-	typedef TPin<Portk, 6> Pk6;
-	typedef TPin<Portk, 7> Pk7;
+	DECLARE_PORT_PINS(Portk, Pk)
 	#endif
 
 	#ifdef USE_PORTQ
-	typedef TPin<Portq, 0> Pq0;
-	typedef TPin<Portq, 1> Pq1;
-	typedef TPin<Portq, 2> Pq2;
-	typedef TPin<Portq, 3> Pq3;
-	typedef TPin<Portq, 4> Pq4;
-	typedef TPin<Portq, 5> Pq5;
-	typedef TPin<Portq, 6> Pq6;
-	typedef TPin<Portq, 7> Pq7;
+	DECLARE_PORT_PINS(Portq, Pq)
 	#endif
 
 	#ifdef USE_PORTR
-	typedef TPin<Portr, 0> Pr0;
-	typedef TPin<Portr, 1> Pr1;
-	typedef TPin<Portr, 2> Pr2;
-	typedef TPin<Portr, 3> Pr3;
-	typedef TPin<Portr, 4> Pr4;
-	typedef TPin<Portr, 5> Pr5;
-	typedef TPin<Portr, 6> Pr6;
-	typedef TPin<Portr, 7> Pr7;
+	DECLARE_PORT_PINS(Portr, Pr)
 	#endif
+
+	#ifdef USE_PORT0
+	DECLARE_PORT_PINS(Port0, P0_)
+	#endif
+
+	#ifdef USE_PORT1
+	DECLARE_PORT_PINS(Port1, P2_)
+	#endif
+
+	#ifdef USE_PORT2
+	DECLARE_PORT_PINS(Port2, P4_)
+	#endif
+
+	#ifdef USE_PORT3
+	DECLARE_PORT_PINS(Port3, P3_)
+	#endif
+
+	#ifdef USE_PORT4
+	DECLARE_PORT_PINS(Port4, P4_)
+	#endif
+
+	#ifdef USE_PORT5
+	DECLARE_PORT_PINS(Port5, P5_)
+	#endif
+
+	#ifdef USE_PORT6
+	DECLARE_PORT_PINS(Port6, P6_)
+	#endif
+
+	#ifdef USE_PORT7
+	DECLARE_PORT_PINS(Port7, P7_)
+	#endif
+
+	#ifdef USE_PORT8
+	DECLARE_PORT_PINS(Port8, P8_)
+	#endif
+
+	#ifdef USE_PORT9
+	DECLARE_PORT_PINS(Port9, P9_)
+	#endif
+
+	#ifdef USE_PORT10
+	DECLARE_PORT_PINS(Port10, P10_)
+	#endif
+
 }
