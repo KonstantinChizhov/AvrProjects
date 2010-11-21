9af67dfaf6018554aaf6e814806412cc8a569481
Konstantin Chizhov <kchizhov@griddynamics.com>
2010-11-22 00:22:42 +0300

    encoder experiments

--- EncoderTests/gcc/EncoderTest.c
+++ EncoderTests/gcc/EncoderTest.c
@@ -5,6 +5,43 @@
 /*
 static uint8_t EncState=0;
 static volatile uint16_t EncValue=0;
+static uint8_t EncState2=0;
+static volatile uint16_t EncValue2=0;
+
+inline static void EncoderCapture(void)
+{
+	uint8_t newValue = PINC & 0x03;
+	uint8_t fullState = newValue | EncState << 2;
+
+	switch(fullState)
+	{
+		case 0x2: case 0x4: case 0xB: case 0xD:
+			EncValue ++;
+		break;
+		case 0x1: case 0x7: case 0x8: case 0xE:
+			EncValue --;
+		break;
+	}
+	EncState = newValue;
+
+	newValue = (PINC & 0x06) >> 2;
+	fullState = newValue | EncState << 2;
+
+	switch(fullState)
+	{
+		case 0x2: case 0x4: case 0xB: case 0xD:
+			EncValue2 ++;
+		break;
+		case 0x1: case 0x7: case 0x8: case 0xE:
+			EncValue2 --;
+		break;
+	}
+	EncState2 = newValue;
+}
+*/
+
+static uint8_t EncState=0;
+static volatile uint16_t EncValue=0;
 
 inline static void EncoderScan(void)
 {
@@ -22,8 +59,9 @@ inline static void EncoderScan(void)
 	}
 	EncState = newValue;
 }
-*/
 
+
+/*
 
 static volatile uint8_t EncState=0;
 static volatile uint16_t EncData=0;
@@ -65,7 +103,7 @@ switch(EncState)
 	EncState = New;
 
 }
-
+*/
 
 	typedef unsigned EncValueType;
 	enum{EncoderChannels = 8};
@@ -89,33 +127,32 @@ switch(EncState)
 		uint8_t fwd  = Detect(_x1, _x2, y1, y2);
 		uint8_t back = Detect(_x2, _x1, y2, y1);
 
-		volatile EncValueType *ptr = EncoderValues;
-		uint8_t mask = 1;
-		do
-		{
-			if(fwd & mask)
-				(*ptr) ++;
-			else 
-			if(back & mask)
-				(*ptr) --;
-			if(mask == 1 << EncoderChannels-1)
-				break;
-			ptr++;
-			mask <<= 1;
-		}while(1);
-
 		_x1 = y1;
 		_x2 = y2;
+
+		volatile EncValueType * ptr = EncoderValues;
+		for(uint8_t i = EncoderChannels; i; --i)
+		{	
+			if(fwd & 1)
+				 (*ptr) ++;
+			else 
+			if(back & 1)
+				(*ptr) --;
+			ptr++;
+			fwd >>= 1;
+			back >>= 1;
+		}
 	}
 
 	inline uint8_t EncRead1()
 	{
-		return PORTC;
+		return PINC;
 	}
 	inline uint8_t EncRead2()
 	{
-		return PORTD;
+		return PIND;
 	}
+
 
 ISR(SIG_OVERFLOW0)
 {
@@ -132,9 +169,11 @@ ISR(SIG_OUTPUT_COMPARE1A)
 
 int main()
 {
-	
+
+
 	while(1)
 	{
+		
 		PORTB = EncData;
 	}
 }
--- EncoderTests/iar/main.cpp
+++ EncoderTests/iar/main.cpp
@@ -3,7 +3,7 @@
 
 static uint8_t EncState=0;
 static volatile uint16_t EncValue=0;
-
+#if 0
 #pragma inline=forced
 inline static void EncoderScan(void)
 {
@@ -27,12 +27,72 @@ __interrupt void TIMER0_OVF()
 {
   EncoderScan();
 }
+#else
+	typedef unsigned EncValueType;
+	enum{EncoderChannels = 8};
+	static volatile EncValueType EncoderValues[EncoderChannels];
+	static uint8_t _x1, _x2;
+
+	static inline uint8_t Detect(uint8_t x1, uint8_t x2, uint8_t y1, uint8_t y2) 
+	{
+		//return ~(x1 | y2) & (x2 ^ y1) | x1 & y2 & ~(x2 & y1);
+		return (x2 ^ y1) & ~(x1 ^ y2);
+	}
+
+	inline uint8_t EncRead1();
+	inline uint8_t EncRead2();
+        
+#pragma inline=forced
+	static inline void EncoderCapture()
+	{
+		uint8_t y1 = EncRead1();
+		uint8_t y2 = EncRead2();
+
+		uint8_t fwd  = Detect(_x1, _x2, y1, y2);
+		uint8_t back = Detect(_x2, _x1, y2, y1);
+
+		_x1 = y1;
+		_x2 = y2;
+
+		volatile EncValueType * ptr = EncoderValues;
+		uint8_t i = EncoderChannels - 1;
+		do
+		{	
+			if(fwd & 1)
+				 (*ptr) ++;
+			else 
+			if(back & 1)
+				(*ptr) --;
+			ptr++;
+			fwd >>= 1;
+			back >>= 1;
+		}while(i--);
+	}
+#pragma inline=forced
+	inline uint8_t EncRead1()
+	{
+		return PINC;
+	}
+#pragma inline=forced
+	inline uint8_t EncRead2()
+	{
+		return PIND;
+	}
+        
+#pragma vector=TIMER1_COMPA_vect
+__interrupt void TIMER1_COMPA()
+{
+  EncoderCapture();
+}
+
+#endif
 
 int main()
 {
   while(1)
   {
-    PORTB = EncValue;
+    //PORTB = EncValue;
+    PORTA = EncoderValues[0];
   }
   return 0;
 }
