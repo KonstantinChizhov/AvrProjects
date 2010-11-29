19d6985caebe9fc2fdb48bbc76c00f4221fc20de
Konstantin Chizhov <kchizhov@griddynamics.com>
2010-11-29 18:30:26 +0300

    rfm70 in progress

--- RFM70/main.cpp
+++ RFM70/main.cpp
@@ -13,7 +13,7 @@ typedef SoftSpi<IO::Pb5, IO::Pb6, IO::Pb7> Spi2;
 
 typedef Rfm70<Spi2, IO::Pb0, IO::Pb1, IO::Pb2> Transiver1;
 
-typedef IO::Pb0 Debug;
+typedef IO::Pa0 Debug;
 
 ISR(USART_RXC_vect)
 {
@@ -33,26 +33,26 @@ int main()
 	Debug::SetDirWrite();
 
 	MyUsart::Init(19200);
-	//MySpi::Init(MySpi::Div16);
-sei();
+	MySpi::Init(MySpi::Div16);
+	//sei();
 	MyFormater formater;
-	formater.PutsP(PSTR("Rfm70 programm\r\n"));
+	formater.PutsP(PSTR("Rfm70 test programm\r\n"));
 
-	Transiver1::Init(MASK_RX_DR | MASK_TX_DS | MASK_MAX_RT | EN_CRC | CRCO | PWR_UP /*| PRIM_RX*/);
+	Transiver1::Init();
 
 	Transiver1::DumpRegs<MyFormater>();
 	Transiver1::DumpRegs1<MyFormater>();
 	//Transiver1::WriteReg(WRITE_REG | CONFIG, 100);
 	//Transiver1::DumpRegs<MyFormater>();
-	uint8_t v = 0;
+	
 	while(1)
 	{
 		//Transiver1::RfChannel(10);
 		//MyUsart::Write(MyUsart::Read());
 		//Transiver1::WriteReg(WRITE_REG | 0, v++);
 		//_delay_us(200);
-	//	MyUsart::Write(Transiver1::ReadReg(READ_REG | 0));
+		//MyUsart::Write(Transiver1::ReadReg(READ_REG | 0));
 
-		_delay_ms(200);
+	//	_delay_ms(200);
 	}
 }
