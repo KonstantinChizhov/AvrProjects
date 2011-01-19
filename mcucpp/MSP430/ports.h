9ec58118338ec09324c7a98f540603bbd513f428
Konstantin Chizhov <kchizhov@griddynamics.com>
2011-01-19 23:08:47 +0300

    MSP Ports refactoring

--- mcucpp/MSP430/ports.h
+++ mcucpp/MSP430/ports.h
@@ -105,99 +105,177 @@ namespace IO
 					return Out;
 				}
 			}
+			
+			template<GenericConfiguration config>
+			struct MapConfigurationConst
+			{
+				static const Configuration value = Out;
+			};
 	};
 	
-	#define MAKE_PORT(portName, dirName, pinName, selectName, interruptName, interruptEdge, resistorEnable, className, ID) \
-		class className :public NativePortBase{\
-		public:\
-			static void Write(DataT value)\
-			{\
-				portName = value;\
-			}\
-			static void ClearAndSet(DataT clearMask, DataT value)\
-			{\
-                portName &= ~clearMask;\
-				portName |= value;\
-			}\
-			static DataT Read()\
-			{\
-				return portName;\
-			}\
-			static void Set(DataT value)\
-			{\
-				portName |= value;\
-			}\
-			static void Clear(DataT value)\
-			{\
-				portName &= ~value;\
-			}\
-			static void Toggle(DataT value)\
-			{\
-				portName ^= value;\
-			}\
-			static DataT PinRead()\
-			{\
-				return pinName;\
-			}\
-			template<unsigned pin>\
-			static void SetPinConfiguration(Configuration configuration)\
-			{\
-				BOOST_STATIC_ASSERT(pin < Width);\
-				if(configuration & DirBit)\
-					dirName |= (1 << pin);\
-				else\
-					dirName &= ~(1 << pin);\
-				if(configuration & AltSelBit)\
-					selectName |= (1 << pin);\
-				else\
-					selectName &= ~(1 << pin);\
-				if(configuration & ResEnBit)\
-					resistorEnable |= (1 << pin);\
-				else\
-					resistorEnable &= ~(1 << pin);\
-			}\
-			static void SetConfiguration(DataT mask, Configuration configuration)\
-			{\
-				if(configuration & DirBit)\
-					dirName |= mask;\
-				else\
-					dirName &= ~mask;\
-				if(configuration & AltSelBit)\
-					selectName |= mask;\
-				else\
-					selectName &= ~mask;\
-				if(configuration & ResEnBit)\
-					resistorEnable |= mask;\
-				else\
-					resistorEnable &= ~mask;\
-			}\
-			enum{Id = ID};\
+	template<> struct NativePortBase::MapConfigurationConst<GpioBase::In>{static const Configuration value = In;};
+	template<> struct NativePortBase::MapConfigurationConst<GpioBase::AnalogIn>{static const Configuration value = AnalogIn;};
+	template<> struct NativePortBase::MapConfigurationConst<GpioBase::OpenDrainOut>{static const Configuration value = Out;};
+	template<> struct NativePortBase::MapConfigurationConst<GpioBase::AltOut>{static const Configuration value = AltOut;};
+	template<> struct NativePortBase::MapConfigurationConst<GpioBase::AltOpenDrain>{static const Configuration value = AltOut;};
+	template<> struct NativePortBase::MapConfigurationConst<GpioBase::PullUpOrDownIn>{static const Configuration value = PullUpOrDownIn;};
+	
+	namespace Private
+	{
+		template<class Out, class Dir, class In, class Sel, class Int, class IntEdge, class Ren, int ID>
+		class PortImplementation :public NativePortBase{
+		public:
+			static void Write(DataT value)
+			{
+				Out::Set(value);
+			}
+			static void ClearAndSet(DataT clearMask, DataT value)
+			{
+				Out::AndOr(~clearMask, value);
+			}
+			static DataT Read()
+			{
+				return Out::Get();
+			}
+			static void Set(DataT value)
+			{
+				Out::Or(value);
+			}
+			static void Clear(DataT value)
+			{
+				Out::And(~value);
+			}
+			static void Toggle(DataT value)
+			{
+				Out::Xor(value);
+			}
+			static DataT PinRead()
+			{
+				return In::Get();
+			}
+			
+			// constant interface
+
+			template<DataT clearMask, DataT value>
+			static void ClearAndSet()
+			{
+				Out::AndOr(~clearMask, value);
+			}
+
+			template<DataT value>
+			static void Toggle()
+			{
+				Out::Xor(value);
+			}
+
+			template<DataT value>
+			static void Set()
+			{
+				Out::Or(value);
+			}
+
+			template<DataT value>
+			static void Clear()
+			{
+				Out::And(~value);
+			}
+			
+			// Configuration interface
+			
+			template<unsigned pin>
+			static void SetPinConfiguration(Configuration configuration)
+			{
+				BOOST_STATIC_ASSERT(pin < Width);
+				if((unsigned)configuration & (unsigned)DirBit)
+					Dir::Or(1 << pin);
+				else
+					Dir::And(~(1 << pin));
+				if((unsigned)configuration & (unsigned)AltSelBit)
+					Sel::Or(1 << pin); 
+				else
+					Sel::And(~(1 << pin));
+				if((unsigned)configuration & (unsigned)ResEnBit)
+					Ren::Or(1 << pin); 
+				else
+					Ren::And(~(1 << pin));
+			}
+			static void SetConfiguration(DataT mask, Configuration configuration)
+			{
+				if((unsigned)configuration & (unsigned)DirBit)
+					Dir::Or(mask);
+				else
+					Dir::And(~mask);
+				if((unsigned)configuration & (unsigned)AltSelBit)
+					Sel::Or(mask); 
+				else
+					Sel::And(~mask);
+				if((unsigned)configuration & (unsigned)ResEnBit)
+					Ren::Or(mask); 
+				else
+					Ren::And(~mask);
+			}
+			
+			template<DataT mask, Configuration configuration>
+			static void SetConfiguration()
+			{
+				if((unsigned)configuration & (unsigned)DirBit)
+					Dir::Or(mask);
+				else
+					Dir::And(~mask);
+				if((unsigned)configuration & (unsigned)AltSelBit)
+					Sel::Or(mask); 
+				else
+					Sel::And(~mask);
+				if((unsigned)configuration & (unsigned)ResEnBit)
+					Ren::Or(mask); 
+				else
+					Ren::And(~mask);
+			}
+
+			enum{Id = ID};
 		};
+	}
+	
+	#define MAKE_PORT(portName, dirName, pinName, selectName, interruptName, interruptEdge, resistorEnable, className, ID) \
+	  namespace Private{\
+		IO_REG_WRAPPER(portName, portName ## _t, uint8_t);\
+		IO_REG_WRAPPER(dirName, dirName ## _t, uint8_t);\
+		IO_REG_WRAPPER(pinName, pinName ## _t, uint8_t);\
+		IO_REG_WRAPPER(selectName, selectName ## _t, uint8_t);\
+		IO_REG_WRAPPER(interruptName, interruptName ## _t, uint8_t);\
+		IO_REG_WRAPPER(interruptEdge, interruptEdge ## _t, uint8_t);\
+		IO_REG_WRAPPER(resistorEnable, resistorEnable ## _t, uint8_t);\
+		}\
+		  typedef Private::PortImplementation<\
+				Private::portName ## _t, \
+				Private::dirName ## _t, \
+				Private::pinName ## _t, \
+				Private::selectName ## _t, \
+				Private::interruptName ## _t, \
+				Private::interruptEdge ## _t, \
+				Private::resistorEnable ## _t, \
+				ID> className;
+		
 
 	#ifdef USE_PORT0
 	MAKE_PORT(P0OUT, P0DIR, P0IN, Port0, '0')
 	#endif
 
-
 	#ifdef USE_PORT1
 	MAKE_PORT(P1OUT, P1DIR, P1IN, P1SEL, P1IE, P1IES, P1REN, Port1, '1')
 	#endif
-
 
 	#ifdef USE_PORT2
 	MAKE_PORT(P2OUT, P2DIR, P2IN, P2SEL, P2IE, P2IES, P2REN, Port2, '2')
 	#endif
 
-
 	#ifdef USE_PORT3
 	MAKE_PORT(P3OUT, P3DIR, P3IN, P3SEL, P3IE, P3IES, P3REN, Port3, '3')
 	#endif
 
-
 	#ifdef USE_PORT4
 	MAKE_PORT(P4OUT, P4DIR, P4IN, P4SEL, P4IE, P4IES, P4REN, Port4, '4')
 	#endif
-
 
 	#ifdef USE_PORT5
 	MAKE_PORT(P5OUT, P5DIR, P5IN, P5SEL, P5IE, P5IES, P5REN, Port5, '5')
