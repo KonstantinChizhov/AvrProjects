7aa0708859b0c396181cfaa1b1d707e0c68ae253
Konstantin Chizhov <kchizhov@mirantis.com>
2011-02-10 17:57:13 +0300

    more experiments

--- testcpp/main.cpp
+++ testcpp/main.cpp
@@ -14,39 +14,41 @@ class ListBase
 	public:
 	typedef uint8_t DataT;
 	typedef NativePortBase::Configuration Configuration;
-	virtual void Write(DataT value)=0;
-	virtual void Set(DataT value)=0;
-	virtual void Clear(DataT value)=0;
-	virtual void SetConfiguration(Configuration value, DataT mask)=0;
-	virtual DataT Read()=0;
-	virtual DataT PinRead()=0;
+	void Write(DataT value){_Write(value);}
+	void Set(DataT value){_Set(value);}
+	void Clear(DataT value){_Clear(value);}
+	void SetConfiguration(Configuration value, DataT mask){_SetConfiguration(value, mask);}
+	DataT PinRead(){return _PinRead();}
+	private:
+	virtual void _Write(DataT value)=0;
+	virtual void _Set(DataT value)=0;
+	virtual void _Clear(DataT value)=0;
+	virtual void _SetConfiguration(Configuration value, DataT mask)=0;
+	virtual DataT _PinRead()=0;
 };
 
 template<class List>
 class ListOfPins : public ListBase
 {
-	public:
-	virtual void Write(DataT value)
+	private:
+	virtual void _Write(DataT value)
 	{
 		List::Write(value);
 	}
-	virtual void Set(DataT value)
+	virtual void _Set(DataT value)
 	{	
 		List::Set(value);
 	}
-	virtual void Clear(DataT value)
+	virtual void _Clear(DataT value)
 	{
 		List::Clear(value);
 	}
-	virtual void SetConfiguration(Configuration value, DataT mask)
+	virtual void _SetConfiguration(Configuration value, DataT mask)
 	{
 		List::SetConfiguration(value, mask);
 	}
-	virtual DataT Read()
-	{
-		return List::Read();
-	}
-	virtual DataT PinRead()
+
+	virtual DataT _PinRead()
 	{
 		return List::PinRead();
 	}
@@ -54,55 +56,96 @@ class ListOfPins : public ListBase
 
 
 typedef void (* WriteFuncPtr)(uint8_t value);
+typedef void (* ConfigFunc)(NativePortBase::Configuration config, uint8_t mask);
+typedef uint8_t (* ReadFuncPtr)();
+
+struct Funcs
+{
+	WriteFuncPtr Write;
+	WriteFuncPtr Set;
+	WriteFuncPtr Clear;
+	ConfigFunc Config;
+	ReadFuncPtr Read;
+};
 
 class Bar
 {
 	public:
-	Bar(WriteFuncPtr func)
-	:_func(func)
+	Bar(Funcs &funcs)
+	:_funcs(funcs)
 	{}
 	
 	void Foo()
 	{
-		_func(0x55);
-		_func(1);
-		_func(2);
-		_func(3);
+		_funcs.Config(NativePortBase::Out, 0xff);
+		_funcs.Write(123);
+		_funcs.Set(0x55);
+		_funcs.Clear(0xff);
+		_funcs.Write(_funcs.Read()+1);
 	}
 
-	WriteFuncPtr _func;
+	Funcs &_funcs;
 };
 
 class B
 {
 	public:
-	B(ListBase *list)
+	B(ListBase &list)
 	:_list(list)
 	{}
 	
 	void Foo()
 	{
-		_list->Write(0x55);	
+		_list.SetConfiguration(NativePortBase::Out, 0xff);
+		_list.Write(123);
+		_list.Set(0x55);
+		_list.Clear(0xff);
+		_list.Write(_list.PinRead()+1);
 	}
 
-	ListBase *_list;
+	ListBase &_list;
 };
+
+	Funcs f = 
+	{
+		Pins1::Write,
+		Pins1::Set,
+		Pins1::Clear,
+		Pins1::SetConfiguration,
+		Pins1::PinRead
+	};
+
+	Funcs f2 = 
+	{
+		Pins2::Write,
+		Pins2::Set,
+		Pins2::Clear,
+		Pins2::SetConfiguration,
+		Pins2::PinRead
+	};
+
 
 __attribute((OS_main))
 int main()
 {
 
+#if 0
 
+	Bar bar(f);
+	Bar bar2(f2);
+	bar.Foo();
+	bar2.Foo();
+#else
 	ListOfPins<Pins1> list1;
 	ListOfPins<Pins2> list2;
 
 
-	B b1(&list1);
-	B b2(&list2);
-
+	B b1(list1);
 	b1.Foo();
+	B b2(list2);
 	b2.Foo();
 
+#endif
 	while(1)
 	{}
 }
