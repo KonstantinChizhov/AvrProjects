7a51b398b975a48fff54817a6ca5ef2649b947e7
Konstantin Chizhov <kchizhov@mirantis.com>
2011-03-11 18:18:54 +0300

    progmem experiments

--- TestsAndSamples/IarProgmemTest/main.cpp
+++ TestsAndSamples/IarProgmemTest/main.cpp
@@ -1,37 +1,32 @@
 #include <ioavr.h>
 #include <pgmspace.h>
 #include <stdint.h>
-//__flash char str1[] = "Hello, world"; 
+
+__flash char strInFlash[] = "Hello from flash"; 
+char strInRam[] = "Hello from RAM";
+__eeprom char strInEeprom[] = "Hello from EEPROM";
+
+static inline void UsartWriteByte(char value)
+{
+	while(!UCSRA_UDRE);
+	UDR = value;
+}
 
 template<class T>
-void Put2(const T begin, const T end)
+void UsartPuts(const T * str)
 {
-  	for(T ptr = begin; ptr != end; ++ptr)
-		PORTA = *ptr;
+  	char c;
+ 	while(c = *str++)
+	{
+  		UsartWriteByte(c);
+	}
 }
-
-#define VIA_PTR 0
-
-#if VIA_PTR
-__attribute((noinline))
-inline void Foo(uint64_t *v)
-{
-	(*v)++;
-}
-#endif
-
-uint64_t value = 0;
 
 int main()
 {
-#if VIA_PTR
-	Foo(&value);
-#else
-	value++;
-#endif
-
-//  Put2(str1, str1 + 11);
-  
-  //PORTA = str[0];
-  return 0;
+	UsartPuts(strInFlash);
+	UsartPuts(strInRam);
+	UsartPuts(strInEeprom);
+	
+	return 0;
 }
--- TestsAndSamples/testcpp/main.cpp
+++ TestsAndSamples/testcpp/main.cpp
@@ -2,155 +2,158 @@
 #include <avr/io.h>
 #include "iopins.h"
 #include "pinlist.h"
-
+#include <avr/pgmspace.h>
+#include <avr/eeprom.h>
 
 using namespace IO;
 
-typedef PinList<Pa4, Pa1, Pa6, Pa3>  Pins1;
-typedef PinList<Pa2, Pa5, Pa0, Pa7>  Pins2;
 
-class ListBase
+template<class T, class Accessor>
+class BasePtr
+{
+	typedef BasePtr Self;
+public:
+	BasePtr(T *address=0)
+		:_address(address)
+	{
+	}
+
+	inline Self& operator ++()
+	{
+		_address++;
+		return *this;
+	}
+
+	inline Self operator ++(int)
+	{
+		Self tmp = *this;
+		_address++;
+		return tmp;
+	}
+
+	inline Self& operator --()
+	{
+		_address--;
+		return *this;
+	}
+
+	inline Self operator --(int)
+	{
+		Self tmp = *this;
+		_address--;
+		return tmp;
+	}
+
+	inline Self& operator +=(int value)
+	{
+		_address += value;
+		return *this;
+	}
+
+	inline Self& operator -=(int value)
+	{
+		_address -= value;
+		return *this;
+	}
+
+	inline Self operator +(int value)
+	{
+		return Self(_address + value);
+	}
+
+	inline Self operator -(int value)
+	{
+		return Self(_address - value);
+	}
+
+	inline bool operator !=(const Self &other) const
+	{
+		return _address != other._address;
+	}
+
+	inline bool operator ==(const Self &other) const
+	{
+		return _address == other._address;
+	}
+
+	inline const T operator *()const
+	{
+		union 
+		{
+			T value;
+			uint8_t bytes[sizeof(T)];
+		}data;
+
+		for(unsigned i = 0; i<sizeof(T); ++i)
+			data.bytes[i] = Accessor::Read(_address + i);
+		return data.value;
+	}
+
+private:
+	T * _address;
+};
+
+
+template<class T>
+class FlashPtr :public BasePtr<T, FlashPtr<T> >
 {
 	public:
-	typedef uint8_t DataT;
-	typedef NativePortBase::Configuration Configuration;
-	void Write(DataT value){_Write(value);}
-	void Set(DataT value){_Set(value);}
-	void Clear(DataT value){_Clear(value);}
-	void SetConfiguration(Configuration value, DataT mask){_SetConfiguration(value, mask);}
-	DataT PinRead(){return _PinRead();}
-	private:
-	virtual void _Write(DataT value)=0;
-	virtual void _Set(DataT value)=0;
-	virtual void _Clear(DataT value)=0;
-	virtual void _SetConfiguration(Configuration value, DataT mask)=0;
-	virtual DataT _PinRead()=0;
-};
-
-template<class List>
-class ListOfPins : public ListBase
-{
-	private:
-	virtual void _Write(DataT value)
-	{
-		List::Write(value);
-	}
-	virtual void _Set(DataT value)
-	{	
-		List::Set(value);
-	}
-	virtual void _Clear(DataT value)
-	{
-		List::Clear(value);
-	}
-	virtual void _SetConfiguration(Configuration value, DataT mask)
-	{
-		List::SetConfiguration(value, mask);
-	}
-
-	virtual DataT _PinRead()
-	{
-		return List::PinRead();
-	}
-};
-
-
-typedef void (* WriteFuncPtr)(uint8_t value);
-typedef void (* ConfigFunc)(NativePortBase::Configuration config, uint8_t mask);
-typedef uint8_t (* ReadFuncPtr)();
-
-struct Funcs
-{
-	WriteFuncPtr Write;
-	WriteFuncPtr Set;
-	WriteFuncPtr Clear;
-	ConfigFunc Config;
-	ReadFuncPtr Read;
-};
-
-class Bar
-{
-	public:
-	Bar(Funcs &funcs)
-	:_funcs(funcs)
+	FlashPtr(T *address)
+	:BasePtr<T, FlashPtr<T> >(address)
 	{}
-	
-	void Foo()
-	{
-		_funcs.Config(NativePortBase::Out, 0xff);
-		_funcs.Write(123);
-		_funcs.Set(0x55);
-		_funcs.Clear(0xff);
-		_funcs.Write(_funcs.Read()+1);
-	}
 
-	Funcs &_funcs;
+	static uint8_t Read(T *addr)
+	{
+		return pgm_read_byte((const uint8_t*)addr);
+	}
 };
 
-class B
+template<class T>
+class EepromPtr :public BasePtr<T, EepromPtr<T> >
 {
 	public:
-	B(ListBase &list)
-	:_list(list)
+	EepromPtr(T *address)
+	:BasePtr<T, EepromPtr<T> >(address)
 	{}
-	
-	void Foo()
-	{
-		_list.SetConfiguration(NativePortBase::Out, 0xff);
-		_list.Write(123);
-		_list.Set(0x55);
-		_list.Clear(0xff);
-		_list.Write(_list.PinRead()+1);
-	}
 
-	ListBase &_list;
+	static uint8_t Read(T *addr)
+	{
+		return eeprom_read_byte((const uint8_t*)addr);
+	}
 };
 
-	Funcs f = 
-	{
-		Pins1::Write,
-		Pins1::Set,
-		Pins1::Clear,
-		Pins1::SetConfiguration,
-		Pins1::PinRead
-	};
 
-	Funcs f2 = 
+
+static inline void UsartWriteByte(char value)
+{
+	while(!(UCSRA & (1 << UDRE)));
+	UDR = value;
+}
+
+template<class T>
+void UsartPuts(T str)
+{
+  	char c;
+ 	while((c = *str++))
 	{
-		Pins2::Write,
-		Pins2::Set,
-		Pins2::Clear,
-		Pins2::SetConfiguration,
-		Pins2::PinRead
-	};
+  		UsartWriteByte(c);
+	}
+}
+
+char strInRam[] = "Hello from RAM";
+PROGMEM char strInFlash[] = "Hello from flash"; 
+EEMEM char strInEeprom[] = "Hello from EEPROM";
+
 
 
 __attribute((OS_main))
 int main()
 {
+	const FlashPtr<char> ptrInFlash(strInFlash);
+	const EepromPtr<char> ptrInEeprom(strInEeprom);
 
-#if 0
-
-	Bar bar(f);
-	Bar bar2(f2);
-	bar.Foo();
-	bar2.Foo();
-#else
-	ListOfPins<Pins1> list1;
-	ListOfPins<Pins2> list2;
-
-
-	B b1(list1);
-	b1.Foo();
-	B b2(list2);
-	b2.Foo();
-
-#endif
-	while(1)
-	{}
+	UsartPuts(ptrInFlash);
+	UsartPuts(ptrInEeprom);
+	UsartPuts(strInRam);
 }
 
-extern "C" void __cxa_pure_virtual()
-{
-
-}
